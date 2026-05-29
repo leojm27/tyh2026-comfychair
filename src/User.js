d5fae4e871d7ff2d7a65ac0972ee8b205a97aa55
Leonardo J Morales <leom6313@gmail.com>
2026-05-29 19:00:23 -0300

    feat: User class

--- src/User.js
+++ src/User.js
@@ -9,34 +9,18 @@ class User {
         this.encryptedPassword = hash.update(password).digest('base64');
     }
 
-    /**
-     * Obtener el nombre completo del usuario.
-     * @returns {string} Nombre completo del usuario.
-     */
     getFullName() {
         return this.fullName;
     }
 
-    /**
-     * Obtener la afiliacion del usuario.
-     * @returns {string} Afiliacion del usuario.
-     */
     getAffiliation() {
         return this.affiliation;
     }
 
-    /**
-     * Obtener correo electrónico del usuario.
-     * @returns {string} Correo electrónico del usuario.
-     */
     getEmail() {
         return this.email;
     }
 
-    /**
-     * Obtener contraseña encriptada del usuario.
-     * @returns {string} Contraseña encriptada del usuario.
-     */
     getEncryptedPassword() {
         return this.encryptedPassword;
     }
