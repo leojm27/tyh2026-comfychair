8c5856e677d06de790a23ca8ef745f08fa61c414
Leonardo J Morales <leom6313@gmail.com>
2026-05-29 00:57:23 -0300

    feat: refactor User class

--- src/User.js
+++ src/User.js
@@ -1,8 +1,7 @@
+const crypto = require('crypto');
 
-
-class User{
-    constructor(fullName, affiliation, email, password){
-        let crypto = require('crypto');
+class User {
+    constructor(fullName, affiliation, email, password) {
         const hash = crypto.createHash('sha256');
         this.fullName = fullName;
         this.affiliation = affiliation;
@@ -10,7 +9,35 @@ class User{
         this.encryptedPassword = hash.update(password).digest('base64');
     }
 
-    getEncryptedPassword(){
+    /**
+     * Obtener el nombre completo del usuario.
+     * @returns {string} Nombre completo del usuario.
+     */
+    getFullName() {
+        return this.fullName;
+    }
+
+    /**
+     * Obtener la afiliacion del usuario.
+     * @returns {string} Afiliacion del usuario.
+     */
+    getAffiliation() {
+        return this.affiliation;
+    }
+
+    /**
+     * Obtener correo electrónico del usuario.
+     * @returns {string} Correo electrónico del usuario.
+     */
+    getEmail() {
+        return this.email;
+    }
+
+    /**
+     * Obtener contraseña encriptada del usuario.
+     * @returns {string} Contraseña encriptada del usuario.
+     */
+    getEncryptedPassword() {
         return this.encryptedPassword;
     }
 }
